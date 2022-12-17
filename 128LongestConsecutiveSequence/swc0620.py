82035f5289c610e44f26a0a42cd523ef5e6d504c
swc0620 <pinetreewave@naver.com>
2022-12-18 07:30:51 +0900

    128LongestConsecutiveSequence

--- 128LongestConsecutiveSequence/swc0620.py
+++ 128LongestConsecutiveSequence/swc0620.py
@@ -1,20 +1,13 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
-        nums = sorted(list(set(nums)))
-        res = 1
-        start = 0
-        end = 0
-
-        for idx in range(1, len(nums)):
-            if nums[idx-1]+1 == nums[idx]:
-                end = idx
-            else:
-                length = end - start + 1
-                res = max(res, length)
-                start = end = idx
-        length = end - start + 1
-        res = max(res, length)
-        return res
+        nums = set(nums)
+        longest = 0
+        for num in nums:
+            if num - 1 not in nums:
+                cur_num = num
+                current = 1
+                while cur_num + 1 in nums:
+                    cur_num += 1
+                    current += 1
+                longest = max(current, longest)
+        return longest
